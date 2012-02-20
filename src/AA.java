8179ed95af2720ef45499163f856ce519ad99709
Oleg Shpynov <oleg.shpynov@jebrains.com>
2012-02-20 14:21:23 +0400

    AA task solution

---
+++ src/AA.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+/**
+ * @author oleg
+ * @date 2/20/12
+ */
+public class AA {
+  public static void main(String[] args) {
+    final Scanner scanner = new Scanner(System.in);
+    System.out.println(scanner.nextLong() + scanner.nextLong());
+  }
+}
