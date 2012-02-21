34909804e08439bb4f4776a08388d97e76c715d1
Oleg Shpynov <oleg.shpynov@jebrains.com>
2012-02-21 22:59:10 +0400

    Don't waste time on classes creation

--- src/F.java
+++ src/F.java
@@ -6,67 +6,54 @@ import java.util.Scanner;
  * @date 2/21/12
  */
 public class F {
-  static class Data {
-    public int value;
-    // -1 0 or 1
-    public int direction;
 
-    Data(final int value, int direction) {
-      this.value = value;
-      this.direction = direction;
-    }
-
-    @Override
-    public String toString() {
-      return String.valueOf(value) + " " + direction;
-    }
-  }
   public static void main(String[] args) {
     final Scanner scanner = new Scanner(new InputStreamReader(System.in));
     final char[] s1 = scanner.nextLine().toCharArray();
     final char[] s2 = scanner.nextLine().toCharArray();
 
-    final Data[][] matrix = new Data[s1.length + 1][s2.length + 1];
+    final int[][][] matrix = new int[s1.length + 1][s2.length + 1][2];
     for (int i=0;i<s1.length+1;i++) {
-      matrix[i][0] = new Data(i, -1);
+      matrix[i][0][0] = i;
+      matrix[i][0][1] = -1;
     }
     for (int j=0;j<s2.length+1;j++) {
-      matrix[0][j] = new Data(j, 1);
+      matrix[0][j][0] = j;
+      matrix[0][j][1] = 1;
     }
 
     for (int i=1;i<=s1.length;i++) {
       for (int j=1;j<=s2.length;j++) {
-        final Data d = new Data(Integer.MAX_VALUE, 0);
-        matrix[i][j] = d;
+        matrix[i][j][0] = Integer.MAX_VALUE;
         int value;
         // match or replace
-        value = matrix[i-1][j-1].value;
+        value = matrix[i-1][j-1][0];
         if (s1[i-1] != s2[j-1]) {
           value++;
         }
-        d.value = value;
-        d.direction = 0;
+        matrix[i][j][0] = value;
+        matrix[i][j][1] = 0;
         // delete
-        value = matrix[i-1][j].value + 1;
-        if (value < d.value) {
-          d.value = value;
-          d.direction = -1;
+        value = matrix[i-1][j][0] + 1;
+        if (value < matrix[i][j][0]) {
+          matrix[i][j][0] = value;
+          matrix[i][j][1] = -1;
         }
         // delete
-        value = matrix[i][j-1].value + 1;
-        if (value < d.value) {
-          d.value = value;
-          d.direction = 1;
+        value = matrix[i][j-1][0] + 1;
+        if (value < matrix[i][j][0]) {
+          matrix[i][j][0] = value;
+          matrix[i][j][1] = 1;
         }
       }
     }
-    System.out.println(matrix[s1.length][s2.length].value);
+    System.out.println(matrix[s1.length][s2.length][0]);
     final StringBuilder builder1 = new StringBuilder();
     final StringBuilder builder2 = new StringBuilder();
     int i = s1.length;
     int j = s2.length;
     while (i > 0 || j > 0) {
-      switch (matrix[i][j].direction) {
+      switch (matrix[i][j][1]) {
         case 0: builder1.insert(0, s1[i-1]); builder2.insert(0, s2[j-1]); i--; j--; break;
         case -1: builder1.insert(0, s1[i-1]); builder2.insert(0, '-'); i--; break;
         case 1: builder1.insert(0, '-'); builder2.insert(0, s2[j-1]); j--; break;
