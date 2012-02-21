1aa919955cfc949c8641243fd726baf2188ce66f
Oleg Shpynov <oleg.shpynov@jebrains.com>
2012-02-21 13:37:17 +0400

    C solution

--- src/C.java
+++ src/C.java
@@ -5,29 +5,30 @@ import java.io.*;
  * @date 2/20/12
  */
 public class C {
-  static enum AminoAcids {
+  static enum AminoAcid {
     Phenylalanine('F', "ttt", "ttc"),
     Leucine('L', "tta", "ttg", "ctt", "ctc", "cta", "ctg"),
     Isoleucine('I', "att", "atc", "ata"),
     Methionine('M', "atg"),
-    Valine('V'),
-    Serine('S'),
-    Proline('P'),
-    Threonine('T'),
-    Alanine('A'),
-    Tyrosine('Y'),
-    Histidine('H'),
-    Glutamine('G'),
-    Asparagine('N'),
-    Lysine('K'),
-    AsparticAcid('D'),
-    GlutamaticAcid('E'),
-    Cysteine('C'),
-    Arginine('R'),
-    Glycine('G'),
-    STOP(' ');
+    Valine('V', "gtt", "gtc", "gta", "gtg"),
+    Serine('S', "tct", "tcc", "tca", "tcg", "agt", "agc"),
+    Proline('P', "cct", "ccc", "cca", "ccg"),
+    Threonine('T', "act", "acc", "aca", "acg"),
+    Alanine('A', "gct", "gcc", "gca", "gcg"),
+    Tyrosine('Y', "tat", "tac"),
+    Histidine('H', "cat", "cac"),
+    Glutamine('Q', "caa", "cag"),
+    Asparagine('N', "aat", "aac"),
+    Lysine('K', "aaa", "aag"),
+    AsparticAcid('D', "gat", "gac"),
+    GlutamaticAcid('E', "gaa", "gag"),
+    Cysteine('C', "tgt", "tgc"),
+    Tryptophan('W', "tgg"),
+    Arginine('R', "cgt", "cgc", "cga", "cgg", "aga", "agg"),
+    Glycine('G', "ggt", "ggc", "gga", "ggg"),
+    STOP(' ', "taa", "tag", "tga");
 
-    AminoAcids(final char name, final String ... codes) {
+    AminoAcid(final char name, final String... codes) {
       myShortName = name;
       myCodes = codes;
     }
@@ -40,17 +41,24 @@ public class C {
     private final String[] myCodes;
 
     // Fill the table
-    private static AminoAcids[][][] ourTable = new AminoAcids[4][4][4];
+    private static AminoAcid[][][] ourTable = new AminoAcid[4][4][4];
     static {
-      for (AminoAcids aminoAcid : values()) {
-        for (String myCode : aminoAcid.myCodes) {
-          ourTable[byChar(myCode.charAt(0))][byChar(myCode.charAt(1))][byChar(myCode.charAt(2))]= aminoAcid;
+      int count = 0;
+      for (AminoAcid aminoAcid : values()) {
+        for (String code : aminoAcid.myCodes) {
+          ourTable[byChar(code.charAt(0))][byChar(code.charAt(1))][byChar(code.charAt(2))]= aminoAcid;
+          count++;
         }
       }
+      assert count == 64;
     }
 
     private char getName(){
       return myShortName;
+    }
+
+    public static AminoAcid getByCodon(final char[] chars){
+      return ourTable[byChar(chars[0])][byChar(chars[1])][byChar(chars[2])];
     }
   }
 
@@ -59,28 +67,38 @@ public class C {
     final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
     final char[] codon = new char[3];
     int i = 0;
+    boolean startCodonSeen=false;
     try {
       while (true) {
         codon[0] = readNonWhitespace(reader);
         codon[1] = readNonWhitespace(reader);
         codon[2] = readNonWhitespace(reader);
-        if (!process(codon, writer)) {
+        final AminoAcid acid = AminoAcid.getByCodon(codon);
+        if (!startCodonSeen && acid == AminoAcid.Methionine) {
+          startCodonSeen = true;
+        }
+        if (!startCodonSeen) {
+          continue;
+        }
+        if (acid == AminoAcid.STOP){
           return;
         }
+        writer.write(acid.myShortName);
       }
     } catch (IOException e) {
       // ignore
+    } finally {
+      try {
+        writer.flush();
+      } catch (IOException e) {
+        // Ignore
+      }
     }
-
-  }
-
-  private static boolean process(final char[] codon, final BufferedWriter writer) {
-
   }
 
   private static char readNonWhitespace(final BufferedReader reader) throws IOException {
     char c = (char) reader.read();
-    while (c == ' '){
+    while (c == ' ' || c == '\n' || c == '\t'){
       c = (char)reader.read();
     }
     return Character.toLowerCase(c);
@@ -90,8 +108,9 @@ public class C {
     switch (c) {
       case 'a' : return 0;
       case 't':  return 1;
-      case 'g': return 2;
+      case 'g':  return 2;
+      case 'c':  return 3;
     }
-    return 3;
+    throw new IllegalArgumentException("Wrong character: " + c);
   }
 }
